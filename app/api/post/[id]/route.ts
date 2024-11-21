bf3c8d5eea1d252f217c7c9a92fe1c4ddf93baa7
Koki <raiden0911kame@gmail.com>
2024-11-21 10:28:29 +0900

    test22

--- app/api/post/[id]/route.ts
+++ app/api/post/[id]/route.ts
@@ -1,13 +1,13 @@
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 import prisma from "../../../../lib/prismaClient";
 
 //データベースの中から任意のパラメーターの情報を入手し、フロントエンドに渡すコンポーネント
 //非同期処理
 export async function GET(
-    req: Request,
-    {params}:{params:{bbsid:string}}
+    req: NextRequest,
+    {params}:{params:{id:string}}
 ){
-    const bbsId = params.bbsid;
+    const bbsId = params.id;
     const bbsDetailData = await prisma.post.findUnique({
         where:{
             id: parseInt(bbsId),
